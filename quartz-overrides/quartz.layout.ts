e7857f7256ec24d2b318908de4cc1fb25a0770a1
Coconuteagle <rlatngu21@gmail.com>
2026-04-07 16:57:02 +0900

    style: adopt knowledge constellation ui

--- quartz-overrides/quartz.layout.ts
+++ quartz-overrides/quartz.layout.ts
@@ -3,55 +3,24 @@ import * as Component from "./quartz/components"
 
 const githubRepo = process.env.GITHUB_REPOSITORY
 const githubUrl = githubRepo ? `https://github.com/${githubRepo}` : "https://github.com/jackyzha0/quartz"
-const localGraphPanel = Component.Graph({
-  localGraph: {
-    depth: 2,
-    scale: 1.16,
-    repelForce: 0.7,
-    centerForce: 0.22,
-    linkDistance: 46,
-    fontSize: 0.78,
-    opacityScale: 1.18,
-    showTags: false,
-    focusOnHover: true,
-  },
-  globalGraph: {
-    scale: 1.02,
-    repelForce: 0.58,
-    centerForce: 0.18,
-    linkDistance: 44,
-    fontSize: 0.78,
-    opacityScale: 1.15,
-    showTags: false,
-    focusOnHover: true,
-  },
+
+const sidebarControls = Component.Flex({
+  components: [
+    {
+      Component: Component.Search(),
+      grow: true,
+    },
+    { Component: Component.Darkmode() },
+    { Component: Component.ReaderMode() },
+  ],
 })
 
-const homeGraphPanel = Component.Graph({
-  localGraph: {
-    depth: -1,
-    scale: 0.94,
-    repelForce: 0.58,
-    centerForce: 0.16,
-    linkDistance: 44,
-    fontSize: 0.76,
-    opacityScale: 1.12,
-    showTags: false,
-    focusOnHover: true,
-    enableRadial: true,
-  },
-  globalGraph: {
-    scale: 1,
-    repelForce: 0.58,
-    centerForce: 0.16,
-    linkDistance: 44,
-    fontSize: 0.76,
-    opacityScale: 1.12,
-    showTags: false,
-    focusOnHover: true,
-    enableRadial: true,
-  },
-})
+const leftSidebar = [Component.PageTitle(), sidebarControls, Component.Explorer()]
+const rightSidebar = [
+  Component.Graph(),
+  Component.DesktopOnly(Component.TableOfContents()),
+  Component.Backlinks(),
+]
 
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
@@ -76,56 +45,17 @@ export const defaultContentPageLayout: PageLayout = {
     Component.ContentMeta(),
     Component.TagList(),
   ],
-  left: [
-    Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
-    Component.Flex({
-      components: [
-        { Component: Component.Search(), grow: true },
-        { Component: Component.Darkmode() },
-        { Component: Component.ReaderMode() },
-      ],
-    }),
-    Component.Explorer(),
-  ],
-  right: [
-    Component.ConditionalRender({
-      component: homeGraphPanel,
-      condition: (page) => page.fileData.slug === "index",
-    }),
-    Component.ConditionalRender({
-      component: localGraphPanel,
-      condition: (page) => page.fileData.slug !== "index",
-    }),
-    Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
-  ],
+  left: leftSidebar,
+  right: rightSidebar,
 }
 
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta(), Component.TagList()],
-  left: [
-    Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
-    Component.Flex({
-      components: [
-        { Component: Component.Search(), grow: true },
-        { Component: Component.Darkmode() },
-        { Component: Component.ReaderMode() },
-      ],
-    }),
-    Component.Explorer(),
+  beforeBody: [
+    Component.Breadcrumbs(),
+    Component.ArticleTitle(),
+    Component.ContentMeta(),
+    Component.TagList(),
   ],
-  right: [
-    Component.ConditionalRender({
-      component: homeGraphPanel,
-      condition: (page) => page.fileData.slug === "index",
-    }),
-    Component.ConditionalRender({
-      component: localGraphPanel,
-      condition: (page) => page.fileData.slug !== "index",
-    }),
-    Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
-  ],
+  left: leftSidebar,
+  right: rightSidebar,
 }
